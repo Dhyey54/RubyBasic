a8ace11029ea434b791ad4d853c34f2d88d62f29
Dhyey Sapara <dhyey@simformsolutions.com>
2023-02-23 14:00:18 +0530

    Made changes to hashes and added exception handling

--- Hashes.rb
+++ Hashes.rb
@@ -2,4 +2,22 @@ user = {:first_name => "Sammy", :last_name => "Shark"}
 puts user
 
 print user[:first_name] # "Sammy"
-print user[:last_name] # "Shark"
+puts user[:last_name] # "Shark"
+
+a0 = [ :foo, :bar ]
+a1 = [ :baz, :bat ]
+h = {a0 => 0, a1 => 1}
+puts h.include?(a0)
+puts h[a0]
+puts a0.hash
+
+a0[0] = :dash
+puts a0.hash
+
+puts h.include?(a0)
+p h[a0]
+puts a0.hash
+
+h.rehash
+puts h.include?(a0)
+p h[a0]
